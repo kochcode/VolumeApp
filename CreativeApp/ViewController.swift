4aaa7a223b1be0e663c39c8d9557dbafe53052f2
Kevin Koch <kevin.koch23@d155.org>
2022-09-08 08:09:56 -0500

    Initial Commit

--- CreativeApp/ViewController.swift
+++ CreativeApp/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageOutlet: UIImageView!
+    @IBOutlet weak var imageButtonOutlet: UIButton!
+    @IBOutlet weak var lengthOutlet: UITextField!
+    @IBOutlet weak var widthOutlet: UITextField!
+    @IBOutlet weak var heightOutlet: UITextField!
+    @IBOutlet weak var volumeOutlet: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func calc(_ sender: UIButton) {
+        volumeOutlet.text = ("Your volume is: \(((Double(lengthOutlet.text!)! * Double(widthOutlet.text!)! * Double(heightOutlet.text!)!)/3)) units")
+    }
+    
+    @IBAction func showImage(_ sender: UIButton) {
+        if( imageButtonOutlet.titleLabel!.text == "Show Image"){
+        imageOutlet.isHidden = false
+        imageButtonOutlet.setTitle("Hide Image", for: .normal)
+        }
+        if( imageButtonOutlet.titleLabel!.text == "Hide Image"){
+        imageOutlet.isHidden = true
+        imageButtonOutlet.setTitle("Show Image", for: .normal)
+        }
+        
+    }
+    
 
 }
 
